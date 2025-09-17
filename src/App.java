3f6beea72a75435b53b9d95ae3a7e4e486f72f36
jkat74nti <110852696+jkat74nti@users.noreply.github.com>
2025-09-17 12:42:30 +0200

    Test klar

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Skapa en app som
+        // Skapar en array med 10 slumptal
+        // Hittar det största talet i arrayen
+    }
+}
